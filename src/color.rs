06a78f6d19274d08961dc7e6a7b2d56ed0e6d911
Karibash <y.karibayashi@gmail.com>
2024-09-19 15:30:25 +0900

    feature: Add a struct that defines the color

---
+++ src/color.rs
@@ -0,0 +1,27 @@
+pub enum Color {
+    Black,
+    Gray,
+    Blue,
+    Green,
+    Cyan,
+    Red,
+    Magenta,
+    Orange,
+    Yellow,
+}
+
+impl From<Color> for &str {
+    fn from(color: Color) -> &'static str {
+        match color {
+            Color::Black => "\x1b[48;2;000;000;000m　",
+            Color::Gray => "\x1b[48;2;127;127;127m　",
+            Color::Blue => "\x1b[48;2;000;000;255m　",
+            Color::Green => "	\x1b[48;2;000;255;000m　",
+            Color::Cyan => "\x1b[48;2;000;255;255m　",
+            Color::Red => "\x1b[48;2;255;000;000m　",
+            Color::Magenta => "\x1b[48;2;255;000;255m　",
+            Color::Orange => "\x1b[48;2;255;127;000m　",
+            Color::Yellow => "\x1b[48;2;255;255;000m　",
+        }
+    }
+}
