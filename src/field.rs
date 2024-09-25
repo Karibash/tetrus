5bcf0bf8186d50e961e7c5878abe1f5a91138cd9
Karibash <y.karibayashi@gmail.com>
2024-09-25 15:37:38 +0900

    feature: Add a field to determine whether or not a collision determination should be made

--- src/field.rs
+++ src/field.rs
@@ -5,14 +5,36 @@ const WIDTH: usize = 13;
 const HEIGHT: usize = 21;
 
 #[derive(Clone, Copy)]
+struct FieldCell {
+    color: Color,
+    has_collision: bool,
+}
+
+impl FieldCell {
+    fn empty() -> Self {
+        Self {
+            color: Color::Black,
+            has_collision: false,
+        }
+    }
+
+    fn block(color: Color) -> Self {
+        Self {
+            color,
+            has_collision: true,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
 pub struct Field {
-    blocks: [[Color; WIDTH]; HEIGHT],
+    blocks: [[FieldCell; WIDTH]; HEIGHT],
 }
 
 impl Default for Field {
     fn default() -> Self {
-        let row = [Color::Black; WIDTH - 2];
-        let mut blocks = [[Color::Gray; WIDTH]; HEIGHT];
+        let row = [FieldCell::empty(); WIDTH - 2];
+        let mut blocks = [[FieldCell::block(Color::Gray); WIDTH]; HEIGHT];
         for block in &mut blocks[..HEIGHT - 1] {
             block[1..WIDTH - 1].copy_from_slice(&row);
         }
@@ -24,8 +46,8 @@ impl From<Field> for Vec<Vec<&'static str>> {
     fn from(value: Field) -> Self {
         value
             .blocks
-            .into_iter()
-            .map(|row| row.into_iter().map(Into::into).collect())
+            .iter()
+            .map(|row| row.iter().map(|cell| cell.color.into()).collect())
             .collect()
     }
 }
